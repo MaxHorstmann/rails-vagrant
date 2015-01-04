6cbc771869240ba128cb618dc8c1151b0b16bac7
Max Horstmann <max.horstmann@gmail.com>
2015-01-03 23:00:36 -0500

    add postgres 9.4

--- provision.sh
+++ provision.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 # see https://gorails.com/setup/ubuntu/14.04
 
+echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
 apt-get update
 apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
+apt-get -y install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
 apt-get -y install nodejs
+apt-get -y install postgresql-9.4
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+
+source /usr/local/rvm/scripts/rvm
+echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc
+
+rvm install 2.1.5
+rvm use 2.1.5 --default
 
 # echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 
 # gem install rails
+
